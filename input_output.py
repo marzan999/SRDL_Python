d22e807f0de26790e791ee9ce986429a8f834104
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-12-20 10:44:12 +0600

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -2,10 +2,9 @@
 num = input('Enter a number: ')
 print('You Entered:', num)
 
-number = -10.6
-name = "Programiz"
-    
-print(5)
 
+number = -10.6
+name = "Marzan" 
+print(5)
 print(number)
 print(name)
