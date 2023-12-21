8e2b65a8beedeff6f476f66c8d9e4e3899770cd5
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-12-21 11:49:18 +0600

    Create task_2.py

---
+++ task_2.py
@@ -0,0 +1,16 @@
+#2.1.6.2
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+add = num1 + num2
+print('Addition is: ', add)
+
+sub = num1 - num2
+print('Substruction is: ', sub)
+
+mul = num1 * num2
+print('Multiplication is: ', mul)
+
+div = num1 / num2
+print('Division is: ', div)
