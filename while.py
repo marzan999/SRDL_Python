f4ccf74d52c14bcdb4cf1328bc8086b0781d7a37
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-12-25 09:44:07 +0600

    Update while.py

--- while.py
+++ while.py
@@ -4,9 +4,17 @@ while i < 6:
   i += 1'''
 
 
-i = 1
+'''i = 1
 while i < 6:
   print(i)
   if i == 3:
     break
+  i += 1'''
+
+
+i = 0
+while i < 6:
   i += 1
+  if i == 3:
+    continue
+  print(i)
