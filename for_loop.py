d213b9f2baeada3a8ebb57bb4e1e13a797ff1f4b
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-12-26 15:42:36 +0600

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -2,5 +2,12 @@
 for x in fruits:
   print(x)'''
 
-for x in range(2, 30, 3):
-  print(x)
+'''for x in range(2, 30, 3):
+  print(x)'''
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
