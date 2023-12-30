df7881bceefb0aca4396d577b0555cda27156dba
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-12-30 12:17:50 +0600

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,5 +1,9 @@
 #5.1.4.2
 #print only even numbers between 1 to 100 using loop
 
-for number in range(2, 101, 2):
-    print(number)
+'''for number in range(2, 101, 2):
+    print(number)'''
+
+for num in range(1,100):
+    if num%2==0:
+        print (num)
