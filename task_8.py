1be1d15812c46ccfc556f648421f09b47280901e
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2023-12-31 10:53:34 +0600

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,8 +1,11 @@
 #5.1.4.3
 #addition of 1 to 100 using loop
 
-sum = 0 
+'''sum = 0 
 for i in range(1, 101): 
     sum = sum + i 
  
-print(sum)
+print(sum)'''
+
+sum_ = sum(range(1,101)) 
+print(sum_)
