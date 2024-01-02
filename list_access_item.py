111109d8dc7627ba47ae9a846dfed36ea5baf49c
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-01-02 20:03:34 +0600

    Update list_access_item.py

--- list_access_item.py
+++ list_access_item.py
@@ -11,3 +11,9 @@ print(thislist[-1])
 #range of indexes
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
+
+
+#item exist or not
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
