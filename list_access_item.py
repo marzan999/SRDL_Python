74db06972d50eb79b1f1cc9f9936b43a0ebc37d4
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-01-02 19:56:20 +0600

    Update list_access_item.py

--- list_access_item.py
+++ list_access_item.py
@@ -1,9 +1,13 @@
 #positive index
-
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
 
-#negative index
 
+#negative index
 thislist = ["apple", "banana", "cherry"]
 print(thislist[-1])
+
+
+#range of indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
