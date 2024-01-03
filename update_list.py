387d0f134a345577497bcc7bdc1ff2fb86c85876
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-01-03 11:47:00 +0600

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -8,3 +8,10 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
+
+
+#add another list
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
