ce01eba02eeb2936467be43ce4b70a8c4ac9e849
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-01-04 11:12:47 +0600

    Update remove_list_item.py

--- remove_list_item.py
+++ remove_list_item.py
@@ -2,3 +2,9 @@
 thislist = ["apple", "banana", "cherry"]
 thislist.remove("banana")
 print(thislist)
+
+
+#remove specific index
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
