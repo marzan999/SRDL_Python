d4515ef66e33ccbdff7d9c8701313aec52c69587
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-01-04 11:19:06 +0600

    Update remove_list_item.py

--- remove_list_item.py
+++ remove_list_item.py
@@ -14,3 +14,9 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.pop()
 print(thislist)
+
+
+#remove first item
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
