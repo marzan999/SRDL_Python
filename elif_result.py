9e32005046e0534bf84a078ba90434b35af83b15
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-06-11 07:44:39 +0600

    updated

---
+++ elif_result.py
@@ -0,0 +1,16 @@
+score = int(input("Enter Score Grade:"))
+
+if score >= 80:
+        print("Your grade is an A+")
+elif score >= 70:
+        print("Your grade is an A")
+elif score >= 60:
+        print("Your grade is an A-")
+elif score >= 50:
+        print("Your grade is an B")
+elif score >= 40:
+        print("Your grade is an C")
+elif score >= 33:
+        print("Your grade is an D")
+else:
+        print ("Sorry, you are failed")
