8ae69b70f902c039dfbaae147f0609ebdd5cee4b
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-06-26 22:49:31 +0600

    Update function_arguments.py

--- function_arguments.py
+++ function_arguments.py
@@ -1,6 +1,13 @@
-def my_function(fname):
+'''def my_function(fname):
   print(fname + " Refsnes")
 
 my_function("Emil")
 my_function("Tobias")
-my_function("Linus")
+my_function("Linus")'''
+
+
+def add_numbers(a, b):
+    sum = a + b
+    print('Sum:', sum)
+
+add_numbers(2, 3)
