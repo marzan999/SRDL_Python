06905c228f86257a6d6be4e04fb17ebe9e204540
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2024-06-26 22:44:53 +0600

    Update list_exp.py

--- list_exp.py
+++ list_exp.py
@@ -1,5 +1,13 @@
-my_list = [1, 2, 3, 4, 5]
+'''my_list = [1, 2, 3, 4, 5]
 
 while my_list:
     my_list.remove(my_list[0]) 
     print(my_list)
+'''
+
+my_list = [1, 2, 3, 4, 5]
+
+while my_list:
+    my_list.remove(my_list[0])
+
+print(my_list)
